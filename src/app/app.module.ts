4442fbabed5aa5e89189f3ecb6381ab2429e0d14
AMRUTHA001 <amruthamnair001@gmail.com>
2023-04-02 12:24:13 +0530

    march

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeRegService } from './employee-reg.service';
+import { EmployeeRegComponent } from './employee-reg/employee-reg.component';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeRegComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [EmployeeRegService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
